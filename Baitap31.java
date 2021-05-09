46d71b92ffeb66add5cec3864f2c4b80be81ae45
vochautuan <62709262+vochautuan@users.noreply.github.com>
2021-05-09 20:37:01 +0700

    Add files via upload

---
+++ Baitap31.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap31;
+
+import static bai3.pkg1.Bai31.tinhuoc;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Chau Tuan
+ */
+public class Baitap31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner a = new Scanner(System.in);
+        System.out.print("Nhap a: ");
+        int A = a.nextInt();
+        System.out.print("Nhap b: ");
+        int B =  a.nextInt();
+        tinhuoc(A,B);
+    }
+    public static void tinhuoc(int n,int m){
+        int kq = 0;
+        for (int i=1;i<=n;i++)
+        {
+            if (n%i==0 && m%i==0)
+                kq=i;
+        }
+        System.out.print("uoc chung lon nhat la:"+kq );
+    }
+     
+}
